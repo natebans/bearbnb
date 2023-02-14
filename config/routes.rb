cb3e8e463a7f56382889ff33a55359a3ae0c60f1
Nate Banas <nate.banas@gmail.com>
2023-02-15 00:02:25 +0100

    created routes for bears and bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
+  resources :bears
+  # resources :bookings do
+  #   member do
+  #     get :request
+  #     get :confirmation
+  #   end
+  # end
+  get "/bookings/:id/request", to: "bookings#request", as: :request
+  get "/bookings/:id/confirmation", to: "bookings#confirmation", as: :confirmation
 end
