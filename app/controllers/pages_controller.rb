36fd1a16327eec8dde5a9238455d4ae160a0a6c5
Maria Micioni <m.micioni85@gmail.com>
2023-02-17 21:52:55 +0100

    Controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home,:index, :show]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
-  end
-
-  def index
-  end
-
-  def show
   end
 end
--- test/controllers/bears_controller_test.rb
+++ test/controllers/bears_controller_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class BearsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get bears_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get bears_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get bears_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get bears_destroy_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get bears_edit_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get bears_show_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get bears_index_url
+    assert_response :success
+  end
 end
