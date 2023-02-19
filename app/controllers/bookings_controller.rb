726fd3674f6186ca8db3ef48829ccb7622f4aaa7
Maria Micioni <m.micioni85@gmail.com>
2023-02-19 14:40:20 +0100

    Added bookings_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,9 +5,28 @@ class BookingsController < ApplicationController
   before_action :find_bear, only: [ :confirmation, :request ]
 
   def confirmation
+    if @booking.update(status: "confirmed")
+      redirect_to booking_path(@booking), notice: 'Booking confirmed'
+    else
+      render 'confirmation', status: :unprocessable_entity
+    end
   end
 
   def request
+    if @booking.update(status: "requested")
+      redirect_to booking_path(@booking), notice: 'Booking request sent'
+    else
+      render 'request', status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
+  attribute :status, :string
   belongs_to :bear
 
   validates :bear_id, presence: true
+  validates :status, presence: true
 end
