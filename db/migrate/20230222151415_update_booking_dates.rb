95a4d68e4ac2c2d6200d5b9d587bf1717cf17bac
Nate Banas <nate.banas@gmail.com>
2023-02-23 16:35:18 +0100

    updated start_date and end_date to dates, made navbar stick to top of page

--- db/migrate/20230222151415_update_booking_dates.rb
+++ db/migrate/20230222151415_update_booking_dates.rb
@@ -1,5 +1,5 @@
 class UpdateBookingDates < ActiveRecord::Migration[7.0]
   def change
-    change_column :bookings, :start_date, :date
+    change_column :bookings, :start_date, 'date USING CAST(start_date AS date)'
   end
 end
