8f4f3a903e6decfdaf0c1e34b016986b471036a9
Nate Banas <nate.banas@gmail.com>
2023-02-25 01:56:08 +0100

    fixed map code to be able to push to heroku

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,15 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values ={apiKey: String}
   connect() {
-  <div id='map' style='width: 400px; height: 300px;'></div>
-<script>
-  mapboxgl.accessToken = this.apiKeyValue
-  const map = new mapboxgl.Map({
-    container: this.element
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
-</script>
+    mapboxgl.accessToken = this.apiKeyValue
+    const map = new mapboxgl.Map({
+      container: this.element,
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
   }
 }
