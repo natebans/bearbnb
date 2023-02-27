7dbdcefa0ae3fa2f0392f1e567fe00587760aa1a
Nate Banas <nate.banas@gmail.com>
2023-02-27 20:14:31 +0100

    made edit user button work, removed password update from form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-  before_action :set_bear, only: :show
-  before_action :set_user, only: :show
+  # before_action :set_bear, only: :show
+  before_action :set_user, only: [:show, :update]
 
   def show
     authorize @user
+  end
+
+  def update
+    authorize @user
+
+    if @user.update!(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'devise/registrations/edit', status: :unprocessable_entity
+    end
   end
 
   private
@@ -13,6 +23,10 @@ class UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find(params[:id])
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
   end
 end
